a961024f44aada9779624dc34089c8e7a40b2d32
goldenmonster <goldenmonster0602@berkeley.edu>
2017-04-02 16:51:57 +0000

    creating and adding new database and table

--- extweetwordcount/src/bolts/creatdatabase.py
+++ extweetwordcount/src/bolts/creatdatabase.py
@@ -2,13 +2,13 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-# create tcount database
+# first we create tcount database
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
 
 
-# cleanup step - drop first if exists
+# cleanup step - drop first before creating new database
 cur.execute('DROP DATABASE IF EXISTS tcount')
 conn.commit()
 
@@ -20,7 +20,7 @@ cur.close()
 conn.close()
 print 'tcount is created now'
 
-# connect to tcount database and create table
+# connect to tcount database and create table Tweetwordcount
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 cur.execute('''CREATE TABLE Tweetwordcount
