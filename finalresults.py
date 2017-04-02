5db445cacb0d05e5496cdbd5ba3c6bb27c2331b0
goldenmonster <goldenmonster0602@berkeley.edu>
2017-04-02 17:37:43 +0000

    adding finalresults

--- finalresults.py
+++ finalresults.py
@@ -4,24 +4,24 @@ import sys
 arg = sys.argv
 arg.pop(0)
 
+#add some resctrictions on the word length and varaibles 
 if len(arg) > 1:
-    sys.exit('accept at most 1 variable.')
+    sys.exit('No more than 1 variable please.')
 
-no_count = 'for you the are this have not but that your get from and just they who can what about out any when now all was don\'t i\'m how she could with it\'s you\'re can\'t'.split()
-if len(arg) == 1 and (len(arg[0]) < 3 or arg[0] in no_count):
-    print 'words below, or with 1 or 2 characters are not counted, please try another one, like \'happy\''
-    print no_count
-    sys.exit()
+#connect to tcount
 
 import psycopg2
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 
+#when passed a single word as argument:
+
 if len(arg) == 1:
     cur.execute('select count from tweetwordcount where word = %s', (arg[0].lower(),))
-    #print 'run: ' + cur.query
     rtn = cur.fetchone()
     print 'Total number of occurences of "%s": %s' %(arg[0].lower(), '0' if not rtn else str(rtn[0]))
+
+#Running without an argument returns all the words in the stream, and their total count of occurrences, worted alphabetically, one word per line.
 
 if len(arg) == 0:
     cur.execute('select * from Tweetwordcount order by word')
