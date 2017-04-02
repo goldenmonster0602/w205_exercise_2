bc1e239b6b3f260c5c5c0e337ffc3d217385cd53
goldenmonster <goldenmonster0602@berkeley.edu>
2017-04-02 17:13:14 +0000

    adding changes in wordcount to connect database table to postgres

--- extweetwordcount/src/bolts/wordcount.py
+++ extweetwordcount/src/bolts/wordcount.py
@@ -5,32 +5,30 @@ from streamparse.bolt import Bolt
 
 import psycopg2
 
+# creat the counter and connect it with tcount database
+
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
         self.conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
         self.cur = self.conn.cursor()
-        # let's sync our counter with the DB, in case there are any records
-        self.log('syncing with database ...')
+        self.log('connect with tcount')
         self.cur.execute('SELECT * FROM Tweetwordcount')
         records = self.cur.fetchall()
         for rec in records:
             self.counts[rec[0]] = int(rec[1])
         self.conn.commit()
-        self.log('completed syncing with %d words' %self.cur.rowcount)
-
+	self.log('connect with %d  word' %self.cur.rowcount)
 
     def process(self, tup):
         word = tup.values[0]
 
         # Write codes to increment the word count in Postgres
         # Use psycopg to interact with Postgres
-        # Database name: Tcount 
-        # Table name: Tweetwordcount 
-        # you need to create both the database and the table in advance.
 
-        # Increment the local count
+
+        # Increment the local count 
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
 
